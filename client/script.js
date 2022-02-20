6eea2ac5a431499a4762669c4b127323daa90e42
Zack Obied <zack.obied@durham.ac.uk>
2022-02-20 00:39:02 +0000

    push

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-async function start () {
+async function refresh () {
   try {
     const response = await fetch('https://durhack-2022.herokuapp.com/all')
     if (!response.ok) {
@@ -12,16 +12,7 @@ async function start () {
 }
 
 document.getElementById('refresh').addEventListener('click', async function (event) {
-  try {
-    const response = await fetch('https://durhack-2022.herokuapp.com/all')
-    if (!response.ok) {
-      throw new Error('404')
-    }
-    const body = await response.text()
-    updatePage(body)
-  } catch (error) {
-    alert(error)
-  }
+  refresh()
 })
 
 // document.getElementById('edit').addEventListener('click', function (event) {
@@ -36,79 +27,72 @@ document.getElementById('refresh').addEventListener('click', async function (eve
 //   })
 // })
 
-// async function makeRequest (event) {
-//   event.preventDefault()
-//   try {
-//     let response
-//     if (event.target.method === 'post') {
-//       response = await fetch(event.target.action, {
-//         method: event.target.method,
-//         body: new URLSearchParams(new FormData(event.target))
-//       })
-//     } else {
-//       response = await fetch(event.target.action + '?search=' + document.getElementById('text').value)
-//     }
-//     if (!response.ok) {
-//       throw new Error('404')
-//     }
-//     const body = await response.text()
-//     updatePage(body)
-//   } catch (error) {
-//     alert(error)
-//   }
-// }
-
 async function updatePage (body) {
   var queries = JSON.parse(body)
   queries = await order(queries)
-  let content = ''
+  var content = ''
   for (let i = 0; i < queries.length; i++) {
-    content +=
-    `<div class="row" style="margin-bottom: 20px;">
-        <div class="card bg-light" style="width: 100%;box-shadow: 10px 10px 10px lightgray;">
-            <div class="card-body">
-                <h5 class="card-title">${queries[i].phone}</h5>
-                <p class="card-text"><i>${queries[i].text}</i></p>
-            </div>
-        </div>
-    </div>`
+    if (queries[i].done == false) {
+      content +=
+      `<div class="row" style="margin-bottom: 20px;">
+          <div class="card bg-light" style="width: 100%;box-shadow: 10px 10px 10px lightgray;">
+              <div class="card-body">
+                  <h5 class="card-title">${queries[i].phone}</h5>
+                  <p class="card-text"><i>${queries[i].text}</i></p>
+                  <button class="btn btn-outline-success" id="done${i}" type="submit">Done<p hidden>*${queries[i].id}*</p></button>
+              </div>
+          </div>
+      </div>`
+    }
   }
   document.getElementById('content').innerHTML = content
+  for (let i = 0; i < queries.length; i++) {
+    if (queries[i].done == false) {
+      document.getElementById(`done${i}`).addEventListener('click', async function (event) {
+        try {
+          let id = document.getElementById(`done${i}`).innerHTML.split('*')
+          let response = await fetch('https://durhack-2022.herokuapp.com/done', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({'id': id[1]})
+          })
+          if (!response.ok) {
+            throw new Error('404')
+          }
+          const body = await response.text()
+          updatePage(body)
+        } catch (error) {
+          alert(error)
+        }
+      })
+    }
+  }
 }
 
 async function order (queries) {
-  let keywords = [['money','mortgage'],['delete','login'],['question','request']]
+  let keywords = [['missed','payment'],['delete','login'],['question','request']]
   var priorities = []
   for (query of queries) {
-    // var priority = Number.MAX_SAFE_INTEGER
-    // for (word of query.text.split(' ')) {
-    //   if (keywords[0].includes(word.toLowerCase())) {
-    //     priority -= 3
-    //   } else if (keywords[1].includes(word.toLowerCase()) && priority > 2) {
-    //     priority -= 2
-    //   } else if (keywords[2].includes(word.toLowerCase()) && priority > 3) {
-    //     priority -= 1
-    //   }
-    // }
     var score = 0
+    for (word of query.text.split(' ')) {
+      if (keywords[0].includes(word.toLowerCase())) {
+        score -= 0.5
+      } else if (keywords[1].includes(word.toLowerCase()) && priority > 2) {
+        score -= 0.25
+      } else if (keywords[2].includes(word.toLowerCase()) && priority > 3) {
+        score -= 0.1
+      }
+    }
     let sentiment = JSON.parse(await getSentiment(query.text)) // Array of json objects
-    if (sentiment[0].classifications[0].tag_name == 'Positive') {
-      score -= sentiment[0].classifications[0].confidence
+    if (sentiment[0].classifications[0].tag_name == 'Negative') {
+      score -= sentiment[0].classifications[0].confidence*2
     } else if (sentiment[0].classifications[0].tag_name == 'Neutral') {
       score += sentiment[0].classifications[0].confidence/2
-    } else if (sentiment[0].classifications[0].tag_name == 'Neutral') {
+    } else if (sentiment[0].classifications[0].tag_name == 'Positive') {
       score += sentiment[0].classifications[0].confidence
-    }
-    let urgency = JSON.parse(await getUrgency(query.text))
-    if (urgency[0].classifications[0].tag_name == 'Urgent') {
-      score -= urgency[0].classifications[0].confidence
-    } else if (urgency[0].classifications[0].tag_name == 'Not Urgent') {
-      score += urgency[0].classifications[0].confidence
     }
     priorities.push(score)
   }
-  console.log(priorities,queries)
-  var highest = 0
   var swap = true
   while (swap == true) {
     swap = false
@@ -144,21 +128,4 @@ async function getSentiment (text) {
   }
 }
 
-async function getUrgency (text) {
-  try {
-    response = await fetch('https://api.monkeylearn.com/v3/classifiers/cl_Aiu8dfYF/classify/', {
-      method: 'post',
-      body: JSON.stringify({'data': [text]}),
-      headers: {'Authorization': 'Token 94ea2fcc40e7a0ba7f57788297727208e9ab2ff6','Content-Type': 'application/json'}
-    })
-    if (!response.ok) {
-      throw new Error('404')
-    }
-    const body = await response.text()
-    return body
-  } catch (error) {
-    alert(error)
-  }
-}
-
-start()
+refresh()
